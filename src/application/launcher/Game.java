f276dac2c05a7a00c273f0651052e4785b4d4072
ajwhitney23 <ajwhitney@wpi.edu>
2020-08-17 22:42:37 -0400

    path now working, game launches

--- src/application/launcher/Game.java
+++ src/application/launcher/Game.java
@@ -17,34 +17,13 @@ public class Game {
         this.path = path;
     }
 
-    private String parsePath() {
-        String temp = "";
-
-        for(int i = 0; i < path.length(); i++) {
-            if(path.charAt(i) == '\\') {
-                temp = temp + '/';
-            }
-            else {
-                temp = temp + path.charAt(i);
-            }
-        }
-        return temp;
-    }
-
     public void launchGame() throws Exception {
-//        String processString = parsePath();
-//        System.out.println("processed String: " + processString);
-//        try {
-//            //Process p = Runtime.getRuntime().exec("\"/ProgramData/Microsoft/Windows/Start Menu/Programs/Riot Games\"");
-////            Process p = Runtime.getRuntime().exec("\"" + processString + "\"");
-//            System.out.println("path: " + path);
-//            Process p = Runtime.getRuntime().exec(path);
-//            p.waitFor();
-//            System.out.println(name + " launched!");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-        String processString = parsePath();
-        Runtime.getRuntime().exec("\"C:/Program%20Files%20(x86)/Steam/steamapps/common/Tom%20Clancy's%20Rainbow%20Six%20Siege/RainbowSix.exe\"");
+        try {
+            Process p = Runtime.getRuntime().exec(path);
+            p.waitFor();
+            System.out.println(name + " has launched!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
